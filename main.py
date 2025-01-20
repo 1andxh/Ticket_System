8dad4970973f1110075acd8594ef2432bd641e47
1andxh <solomonkingsman@gmail.com>
2025-01-20 12:08:23 +0000

    ticket class created

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import csv
 import sys
 
-class TicketSystem:...
+class TicketSystem:
+    def __init__(self, buyer):
+        self.buyer = buyer
+        self.available_tickets = 0
+        self.user_tickets = {}
+        self.quantity = 0
+
+    def create_ticket(self):
+        self.quantity = input(input("Enter numeber of tickets to create: "))

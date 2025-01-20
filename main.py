bdd097d473011736f913a93d024692a6693d53a2
1andxh <solomonkingsman@gmail.com>
2025-01-20 13:36:00 +0000

    functioning create ticket

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import csv
 import sys
 
 class TicketSystem:
-    def __init__(self, buyer):
-        self.buyer = buyer
+    def __init__(self, owner):
+        self.owner = owner
         self.available_tickets = 0
         self.user_tickets = {}
         self.quantity = 0
 
     def create_ticket(self):
         self.quantity = input(input("Enter numeber of tickets to create: "))
+        self.available_tickets += self.quantity
+        print(f"{self.quantity} tickets created successfully!")
